2bd7312e5d5827fbcca40b3ca57d9bd556c4985d
Shreya01-C <shreya004r@gmail.com>
2026-09-05 21:50:49 +0530

    Trending Movies done

--- src/components/lazyLoadImage/img.js
+++ src/components/lazyLoadImage/img.js
@@ -2,7 +2,6 @@ import React from "react";
 // import {LazyLoadImage} from "react-lazy-load-image"
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-
 const Img = ({ src, className }) => {
     return (
         <LazyLoadImage
